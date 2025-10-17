efd165253a2cf47f5d1fe7e947923f0ed3c3b155
pawankalyan <pawankalyankarri22@gmail.com>
2025-10-17 22:55:30 +0530

    drill down chart is created

--- src/rechartsComponents/BarChartPage.tsx
+++ src/rechartsComponents/BarChartPage.tsx
@@ -1,4 +1,6 @@
 import { barData, type DrillDataType } from "@/chartComponents/ChartData";
+import { Button } from "@/components/ui/button";
+import { cos } from "@amcharts/amcharts5/.internal/core/util/Math";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -13,37 +15,78 @@ import {
 
 const BarChartPage = () => {
   // const chartData = barData as DrillDataType[];
-  const [chartData,setChartData] = useState<DrillDataType[]>([])
-  const mountRef = useRef<boolean>(false)
-  useEffect(()=>{
-    if(mountRef.current) return;
+  const [chartData, setChartData] = useState<DrillDataType[]>([]);
+  const mountRef = useRef<boolean>(false);
+  const [prevCategory,setPrevCategory] = useState<string>("")
+  useEffect(() => {
+    if (mountRef.current) return;
     mountRef.current = true;
-    axios.get("/jsonChartData.json").then(res=>setChartData(res.data)).catch(err=>console.log(err))
-  },[])
-  const navigate = useNavigate()
+    axios
+      .get("/jsonChartData.json")
+      .then((res) => setChartData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  const navigate = useNavigate();
+
+  const findMatchObj = (data:DrillDataType[],category:string):DrillDataType|null => {
+    console.log(category,data)
+    if(data.length === 0) return null;
+    for(let item of data){
+      if(item.category === category) return item;
+      if(item.children){
+        const obj : DrillDataType|null = findMatchObj(item.children,category)
+        if(obj) return obj;
+      }
+    }
+    return null
+  }
 
   const handleClick = (data: any) => {
-    console.log('data',data)
-    const singleData = chartData.filter(item=>item.category === data.category )  // here i am filtering selected field
-    console.log(singleData[0].children) 
-    setChartData(singleData[0].children|| [])    // setting that selected field to chartdata
+    console.log("data", data.children);
+    console.log('data.category',data.category)
+    setPrevCategory(data.category)
+    axios
+      .get("/jsonChartData.json")
+      .then((res) => {
+        const singleData = findMatchObj(res.data,data.category)
+        if (singleData?.children) setChartData(singleData.children);
+        else setChartData(res.data)
+      })
+      .catch((err) => console.log(err));
+
+    // setChartData(data.children||[])
+    // const singleData = chartData.filter(item=>item.category === data.category )  // here i am filtering selected field
+    // console.log(singleData[0].children)
+    // setChartData(singleData[0].children|| [])    // setting that selected field to chartdata
   };
+
+  function handleBack(data: DrillDataType[]) {
+    console.log(data)
+    console.log('prevcategory',prevCategory)
+    const previousData = findMatchObj(data,prevCategory)
+    console.log('prevdata',previousData)
+  }
   // console.log(chartData)
   return (
-    <div style={{ width: "30%", height: 300 }} className="">
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart data={chartData}>
-          <XAxis dataKey="category" />
-          <YAxis />
-          <Tooltip />
-          <Bar
-            dataKey="value"
-            fill="#8884d8"
-            activeBar={false}
-            onClick={handleClick}
-          />
-        </BarChart>
-      </ResponsiveContainer>
+    <div className="w-full h-screen flex justify-around items-center flex-col">
+      <div className="">
+        <Button onClick={() => handleBack(chartData)}>Back</Button>
+      </div>
+      <div className="w-[25%] h-[300px] ">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart data={chartData}>
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Tooltip />
+            <Bar
+              dataKey="value"
+              fill="#8884d8"
+              activeBar={false}
+              onClick={handleClick}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
