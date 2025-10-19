1bfd2f7529be2171a55acbed7b615d7cdf3d999e
pawankalyan <pawankalyankarri22@gmail.com>
2025-10-19 15:58:21 +0530

    back chart details displaying

--- src/rechartsComponents/BarChartPage.tsx
+++ src/rechartsComponents/BarChartPage.tsx
@@ -1,9 +1,7 @@
-import { barData, type DrillDataType } from "@/chartComponents/ChartData";
+import {type DrillDataType } from "@/chartComponents/ChartData";
 import { Button } from "@/components/ui/button";
-import { cos } from "@amcharts/amcharts5/.internal/core/util/Math";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import {
   BarChart,
   Bar,
@@ -16,6 +14,7 @@ import {
 const BarChartPage = () => {
   // const chartData = barData as DrillDataType[];
   const [chartData, setChartData] = useState<DrillDataType[]>([]);
+  const [backCatArray, setBackCatArray] = useState<string[]>([]);
   const mountRef = useRef<boolean>(false);
   const [prevData, setPrevData] = useState<DrillDataType[][]>([]);
   useEffect(() => {
@@ -26,13 +25,12 @@ const BarChartPage = () => {
       .then((res) => setChartData(res.data))
       .catch((err) => console.log(err));
   }, []);
-  const navigate = useNavigate();
 
   const findMatchObj = (
     data: DrillDataType[],
     category: string
   ): DrillDataType | null => {
-    console.log(category, data);
+    // console.log(category, data);
     if (data.length === 0) return null;
     for (let item of data) {
       if (item.category === category) return item;
@@ -45,27 +43,27 @@ const BarChartPage = () => {
   };
 
   const handleClick = (data: any) => {
-    console.log("data", data.children);
-    console.log("data.category", data.category);
+    // console.log("data", data.children);
+    // console.log("data.category", data.category);
     axios
       .get("/jsonChartData.json")
       .then((res) => {
         const singleData = findMatchObj(res.data, data.category);
         if (singleData?.children) {
+          setBackCatArray((prev) => [...prev, singleData.category]);
           setPrevData((prev) => [...prev, chartData]);
           setChartData(singleData.children);
-        } else setChartData(res.data);
+        } else {
+          setChartData(res.data);
+          setBackCatArray([]);
+          setPrevData([]);
+        }
       })
       .catch((err) => console.log(err));
-
-    // setChartData(data.children||[])
-    // const singleData = chartData.filter(item=>item.category === data.category )  // here i am filtering selected field
-    // console.log(singleData[0].children)
-    // setChartData(singleData[0].children|| [])    // setting that selected field to chartdata
   };
 
   function handleBack() {
-    if(prevData.length === 0) return;
+    if (prevData.length === 0) return;
     console.log("prevcategory", prevData);
     setChartData(prevData[prevData.length - 1]);
 
@@ -74,26 +72,28 @@ const BarChartPage = () => {
   // console.log(chartData)
   return (
     <div className="w-full h-screen flex justify-around items-center flex-col">
-      <div className="">
+      <div className=" w-full flex justify-center items-center flex-col ">
         <Button onClick={handleBack}>Back</Button>
+        <div className="flex gap-5 p-5">
+          {backCatArray.map((item, idx) => {
+            return <span key={idx}  className="cursor-pointer">{item}</span>;
+          })}
+        </div>
       </div>
-      <div className="w-[35%] h-[300px] ">
+      <div className="w-[45%] h-[300px] ">
         <ResponsiveContainer width="100%" height="100%">
           <BarChart data={chartData}>
             <XAxis dataKey="category" fontSize={12} />
             <YAxis />
-            <Tooltip  />
+            <Tooltip cursor={false} />{" "}
+            {/* removing hover effect use cursor false */}
             <Bar
               dataKey="value"
               fill="#8884d8"
-              isAnimationActive = {false}
               onClick={handleClick}
               className="cursor-pointer"
               maxBarSize={40}
               radius={[6, 6, 0, 0]}
-              
-              
-
             />
           </BarChart>
         </ResponsiveContainer>
