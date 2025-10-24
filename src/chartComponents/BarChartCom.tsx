58d8a7f4f8d880fa5463e1e4db674d66d52f7c1f
pawankalyan <pawankalyankarri22@gmail.com>
2025-10-24 15:41:40 +0530

    another drill down chart is added

--- src/chartComponents/BarChartCom.tsx
+++ src/chartComponents/BarChartCom.tsx
@@ -7,20 +7,26 @@ import { Button } from "@/components/ui/button";
 import type { DrillDataType } from "@/chartComponents/ChartData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsRotate, faBackward } from "@fortawesome/free-solid-svg-icons";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { toast } from "sonner";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const BarChartCom = () => {
   const chartRef = useRef<HTMLDivElement>(null);
   const rootRef = useRef<am5.Root | null>(null);
-  const mountRef = useRef<boolean>(false)
+  const mountRef = useRef<boolean>(false);
   const [chartData, setChartData] = useState<DrillDataType[]>([]);
   const [prevData, setPrevData] = useState<DrillDataType[][]>([]);
   const [backCatArray, setBackCatArray] = useState<string[]>([]);
 
   useEffect(() => {
-    if(mountRef.current) return 
-    mountRef.current = true
+    if (mountRef.current) return;
+    mountRef.current = true;
     axios
       .get("/jsonChartData.json")
       .then((res) => setChartData(res.data))
@@ -49,38 +55,40 @@ const BarChartCom = () => {
     setBackCatArray(backCatArray.slice(0, -1));
   };
 
-  function handleRefresh(){
+  function handleRefresh() {
     axios
       .get("/jsonChartData.json")
       .then((res) => {
-        setChartData(res.data)
-        setBackCatArray([])
-        setPrevData([])
+        setChartData(res.data);
+        setBackCatArray([]);
+        setPrevData([]);
       })
       .catch((err) => console.log(err));
   }
 
-  function handleBreadCrumbChange(category : string){
+  function handleBreadCrumbChange(category: string) {
     // console.log(category)
     // console.log(chartData)
     // console.log(prevData)
     // console.log(backCatArray)
-   
-    axios.get("/jsonChartData.json").then(res=>{
-      const resobj = findMatchObj(res.data,category)
-      console.log('resobj',resobj)
-      // let matchedIdx = prevData.findIndex(item=>item.some(obj=>obj.category === category)) // finding matched object index
-      let matchedIdx = backCatArray.findIndex(item=>item === category) // finding matched object index
 
-      // console.log('matchedIdx',matchedIdx)
-      if (resobj?.children){
-        setChartData(resobj.children)
-        setPrevData(prevData.slice(0,matchedIdx+1))
-        setBackCatArray(backCatArray.slice(0,matchedIdx+1))
-      } 
-      return
-    }).catch(err=>console.log(err))
-    
+    axios
+      .get("/jsonChartData.json")
+      .then((res) => {
+        const resobj = findMatchObj(res.data, category);
+        console.log("resobj", resobj);
+        // let matchedIdx = prevData.findIndex(item=>item.some(obj=>obj.category === category)) // finding matched object index
+        let matchedIdx = backCatArray.findIndex((item) => item === category); // finding matched object index
+
+        // console.log('matchedIdx',matchedIdx)
+        if (resobj?.children) {
+          setChartData(resobj.children);
+          setPrevData(prevData.slice(0, matchedIdx + 1));
+          setBackCatArray(backCatArray.slice(0, matchedIdx + 1));
+        }
+        return;
+      })
+      .catch((err) => console.log(err));
   }
 
   useLayoutEffect(() => {
@@ -104,7 +112,6 @@ const BarChartCom = () => {
         wheelY: "none",
         paddingLeft: 0,
         paddingRight: 1,
-
       })
     );
 
@@ -112,7 +119,10 @@ const BarChartCom = () => {
     cursor.lineY.set("visible", false);
     cursor.lineX.set("visible", false);
 
-    const xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30,strokeOpacity : 0.1 });
+    const xRenderer = am5xy.AxisRendererX.new(root, {
+      minGridDistance: 30,
+      strokeOpacity: 0.1,
+    });
     xRenderer.labels.template.setAll({
       rotation: -60,
       fontSize: 12,
@@ -129,7 +139,7 @@ const BarChartCom = () => {
     );
 
     // xRenderer.grid.template.setAll({
-    //   visible: false, 
+    //   visible: false,
     // });
 
     const yRenderer = am5xy.AxisRendererY.new(root, { strokeOpacity: 0.1 });
@@ -161,7 +171,7 @@ const BarChartCom = () => {
     });
 
     series.columns.template.adapters.add("fill", (fill, target) => {
-      return chart.get("colors")!.getIndex(series.columns.indexOf(target));  // each grid will get different color 
+      return chart.get("colors")!.getIndex(series.columns.indexOf(target)); // each grid will get different color
     });
 
     series.columns.template.events.on("click", (ev) => {
@@ -181,7 +191,7 @@ const BarChartCom = () => {
           setChartData(matched.children);
           setBackCatArray((prev) => [...prev, category]);
         } else {
-          toast.info("Chart is Refreshed")
+          toast.info("Chart is Refreshed");
           setChartData(res.data);
           setPrevData([]);
           setBackCatArray([]);
@@ -202,26 +212,43 @@ const BarChartCom = () => {
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <div className="flex flex-col items-center mb-4 gap-5">
         <div className="flex items-center justify-center gap-5">
-            {backCatArray.length !== 0 && <FontAwesomeIcon icon={faBackward} onClick={handleBack} className="cursor-pointer"  />}
-            {backCatArray.length>0 && <FontAwesomeIcon icon={faArrowsRotate} onClick={handleRefresh} className="cursor-pointer"  /> }
-            
+          {backCatArray.length !== 0 && (
+            <FontAwesomeIcon
+              icon={faBackward}
+              onClick={handleBack}
+              className="cursor-pointer p-2"
+            />
+          )}
+          {backCatArray.length > 0 && (
+            <FontAwesomeIcon
+              icon={faArrowsRotate}
+              onClick={handleRefresh}
+              className="cursor-pointer p-2"
+            />
+          )}
         </div>
         <Breadcrumb>
           <BreadcrumbList>
             {backCatArray.map((item, idx) => {
               return (
-                <span key={idx} className="flex justify-center items-center gap-2">
-                <BreadcrumbItem  className="cursor-pointer" onClick={()=>handleBreadCrumbChange(item)}>
-                  {item}
-                  
-                </BreadcrumbItem>
-                {idx < backCatArray.length - 1 && <BreadcrumbSeparator />}
+                <span
+                  key={idx}
+                  className="flex justify-center items-center gap-2"
+                >
+                  <BreadcrumbItem
+                    className="cursor-pointer"
+                    onClick={() => handleBreadCrumbChange(item)}
+                  >
+                    {item}
+                  </BreadcrumbItem>
+                  {idx < backCatArray.length - 1 && <BreadcrumbSeparator />}
                 </span>
               );
             })}
           </BreadcrumbList>
         </Breadcrumb>
       </div>
+
       <div ref={chartRef} className="w-[30%] h-[400px]" />
     </div>
   );
